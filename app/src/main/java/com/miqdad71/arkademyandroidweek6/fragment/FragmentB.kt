2f5cf0da3023fd6fb65412a882226743360b2d0c
miqdad71 <miqdad.ihbs71777@gmail.com>
2020-12-18 14:57:51 +0700

    SimpleFragmentActivity.kt

--- app/src/main/java/com/miqdad71/arkademyandroidweek6/fragment/FragmentB.kt
+++ app/src/main/java/com/miqdad71/arkademyandroidweek6/fragment/FragmentB.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.miqdad71.arkademyandroidweek6.R
 import java.util.zip.Inflater
 
-class FragmentB: Fragment() {
+class FragmentB(private val listener: BListener?): Fragment() {
     private lateinit var rootView: View
 
     override fun onCreateView(
@@ -19,5 +20,18 @@ class FragmentB: Fragment() {
         rootView = inflater.inflate(R.layout.fragment_b, container, false)
         return rootView
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnChangeB = rootView.findViewById<Button>(R.id.btn_info_b)
+        btnChangeB.setOnClickListener {
+            //action to passing data
+            listener?.onChangeBclick("B")
+        }
+    }
+
+    interface BListener{
+        fun onChangeBclick(info: String)
     }
 }
--- app/src/main/java/com/miqdad71/arkademyandroidweek6/fragment/SimpleFragmentActivity.kt
+++ app/src/main/java/com/miqdad71/arkademyandroidweek6/fragment/SimpleFragmentActivity.kt
@@ -2,16 +2,22 @@ package com.miqdad71.arkademyandroidweek6.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.textservice.TextInfo
 import android.widget.Button
+import android.widget.TextView
 import com.miqdad71.arkademyandroidweek6.R
 
-class SimpleFragmentActivity : AppCompatActivity() {
+class SimpleFragmentActivity : AppCompatActivity(), FragmentB.BListener {
+
+    private lateinit var textInfo: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_simple_fragment)
 
         val btnSwitchA = findViewById<Button>(R.id.btn_switch_a)
         val btnSwitchB = findViewById<Button>(R.id.btn_switch_b)
+        textInfo = findViewById(R.id.tv_info_fragment)
 
         //Activity to Fragment
         btnSwitchA.setOnClickListener {
@@ -22,10 +28,14 @@ class SimpleFragmentActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction().replace(R.id.fl_container, aFragment).commit()
         }
 
+        //Passing data fragment to activity
         btnSwitchB.setOnClickListener {
-            val bFragment = FragmentB()
-            supportFragmentManager.beginTransaction().replace(R.id.fl_container, bFragment).commit()
-        }
-
+            val bFragment = FragmentB(object : FragmentB.BListener {
+                override fun onChangeBclick(info: String) {
+                    textInfo.text = info
+                }
+                })
+                supportFragmentManager.beginTransaction().replace(R.id.fl_container, bFragment).commit()
+            }
     }
 }
